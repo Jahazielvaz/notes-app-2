9c3e413f452e36a2832e0e4ee4160270ebdb0b54
Your Name <teakowg@gmail.com>
2017-03-05 12:38:50 +0000

    Playing with test doubles for note controller

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -4,6 +4,23 @@ function testNoteControllerCreation() {
   console.log( "testNoteControllerCreation returned " + controller )
 };
 
- 
+function testAddsNotesToDOM() {
+  function NoteListDouble() {
+    return ["This is a test note"];
+  };
+
+  var list = NoteListDouble();
+
+  function NoteListViewDouble(list) {
+    return "<ul><li><div>" + list + "</div></li></ul>"
+  };
+
+  NoteListViewDouble.prototype.conve
+
+  var controller = new NoteController(list)
+  controller.getHTML
+
+
+};
 
 testNoteControllerCreation();
