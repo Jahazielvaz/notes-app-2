b4262a231a395919f36a0e4fb5143c05bffcdf6c
Your Name <teakowg@gmail.com>
2017-03-06 13:26:01 +0000

    Refactored TDD test for creating note link from id

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -9,24 +9,24 @@ function testNoteListConvertsListToHTML() {
   var list = new NoteList();
   list.createNote("hello!");
   var noteView = new NoteListView(list);
-  assert.isTrue( noteView.convertToHTML() === "<ul><li><div>hello!</div></li></ul>"  )
-  console.log( "testNoteListConvertsToHTML returned: " + noteView.convertToHTML() )
+  assert.isTrue( noteView.convertToHTML() === "<ul><li><div>hello!</div></li></ul>"  );
+  console.log( "testNoteListConvertsToHTML returned: " + noteView.convertToHTML() );
 };
 
 function testNoteShowsOnly20Chars() {
   var list = new NoteList();
   list.createNote("hello! I am a veeeerrrry long note!");
   var noteView = new NoteListView(list);
-  assert.isTrue( noteView.convertToHTML() === "<ul><li><div>hello! I am a veeeer</div></li></ul>"  )
-  console.log( "testNoteShowsOnly20Chars returned: " + noteView.convertToHTML() )
+  assert.isTrue( noteView.convertToHTML() === "<ul><li><div>hello! I am a veeeer</div></li></ul>"  );
+  console.log( "testNoteShowsOnly20Chars returned: " + noteView.convertToHTML() );
 };
 
 function testNoteListCreatesNoteLink() {
   var list = new NoteList();
-  list.createNote("note linked url")
-  list.assignNoteId(); // get this id number and add it after hash of url
-  assert.isTrue( list.createLink() === "<a href="#note/0">Note #0</a>" )
-  console.log( "testNoteListCreatesNoteLink returned: " + list.createLink() )
+  list.createNote( "note linked url" )
+  list.assignNoteId();
+  assert.isTrue( list.createLink() === "<a href='#note/0'>Note #0</a>" );
+  console.log( "testNoteListCreatesNoteLink returned: " + list.createLink() );
 };
 
 
@@ -34,4 +34,4 @@ function testNoteListCreatesNoteLink() {
 testNoteListViewCreation();
 testNoteListConvertsListToHTML();
 testNoteShowsOnly20Chars();
-testNoteListLinksNoteToURL();
+testNoteListCreatesNoteLink();
